c4725c3a806fb6b9ac747e61fb1deece1eacf0b5
versa-dev <tothefuture1117@gmail.com>
2020-11-19 05:07:17 +0800

    add navigation to the app

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,6 +16,11 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
-  } 
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 
 }
